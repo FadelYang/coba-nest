4189226eb0044065889a470407b94ec074fc67af
fadelanumah <fadelanumah@gmail.com>
2024-10-21 00:18:48 +0700

    feat(auth): Add Auth Guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core.module';
-import { UsersController } from './controllers/users.controller';
 import { UsersModule } from './modules/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './common/auth.guard';
 
 @Module({
   imports: [
@@ -12,11 +13,18 @@ import { JwtModule } from '@nestjs/jwt';
     UsersModule,
     JwtModule.register({
       global: true,
-      secret: 'super_secret_key',
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '12h' }
     })
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    }
+  ],
+  
 })
 export class AppModule {}
