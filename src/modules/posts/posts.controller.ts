56a78402dd555c65ef685271a9d8ab755e3aca41
fadelanumah <fadelanumah@gmail.com>
2024-10-21 14:30:16 +0700

    feat(posts): Add CRUD Controller

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post, Request } from "@nestjs/common";
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, Request, UseGuards } from "@nestjs/common";
 import { PostsService } from "./posts.service";
 import { Post as CPost } from '@prisma/client';
 import { CreatePostDto } from "./dtos/create-post.dto";
 import { ExpressRequestWithUser } from "../users/interfaces/express-request-with-user.interface";
+import { Public } from "src/common/decorators/public.decorator";
+import { IsMineGuard } from "src/common/is-mine.guard";
 
 @Controller('posts')
 export class PostsController {
@@ -15,5 +17,23 @@ export class PostsController {
     ): Promise <CPost> {
         createPostDto.authorId = req.user.sub
         return this.postsService.createPost(createPostDto)
+    }
+
+    @Public()
+    @Get()
+    getAllPosts(): Promise<CPost[]> {
+        return this.postsService.getAllPosts()
+    }
+
+    @Public()
+    @Get(':id')
+    getPostById(@Param('id', ParseIntPipe) id: number): Promise<CPost> {
+      return this.postsService.getPostById(id);
+    }
+
+    @Patch(':id')
+    @UseGuards(IsMineGuard)
+    async deletePost(@Param('id', ParseIntPipe) id: number): Promise<string> {
+        return this.postsService.deletePost(+id)
     }
 }
