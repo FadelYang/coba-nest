8e56b5aca4cf3bb2c513581ddd9958b4a5463813
fadelanumah <fadelanumah@gmail.com>
2024-10-21 14:41:44 +0700

    feat(posts): Add Post Module In App Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,12 @@ import { UsersModule } from './modules/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './common/auth.guard';
+import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
   imports: [
     CoreModule,
+    PostsModule,
     UsersModule,
     JwtModule.register({
       global: true,
